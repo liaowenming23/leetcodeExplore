fbf9dd86665a3fa1c9cf22797cb7d9a35fe9b5f3
liaowenming <liaowenming23@gmail.com>
2018-08-05 22:13:11 +0800

    Add linkedlist

--- leetcodeExplore/Program.cs
+++ leetcodeExplore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using leetcodeExplore.lib;
 
 namespace leetcodeExplore
 {
@@ -7,6 +8,52 @@ namespace leetcodeExplore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            TestLinkedList();
+            Console.ReadKey();
         }
+
+        static void TestLinkedList(){
+           
+            MyLinkedList l = new MyLinkedList();
+            l.AddAtHead(38);
+            l.AddAtHead(45);
+            l.DeleteAtIndex(2);
+            l.AddAtIndex(1,24);
+            l.AddAtTail(36);
+            l.AddAtIndex(3,72);
+            l.AddAtTail(76);
+            l.AddAtHead(7);
+            l.AddAtHead(36);
+            l.AddAtHead(34);
+            l.AddAtTail(91);
+            l.AddAtTail(69);
+            l.AddAtHead(37);
+            l.AddAtTail(38);
+            l.AddAtTail(4);
+            l.AddAtHead(66);
+            l.AddAtTail(38);
+            l.DeleteAtIndex(14);
+            l.AddAtTail(12);
+            l.AddAtTail(32);
+            var a = l.Get(5);
+            l.AddAtIndex(7,5);
+            l.AddAtHead(74);
+            var b = l.Get(7);
+            var c = l.Get(13);
+    
+            while(l.node != null){
+                Console.WriteLine(l.node.val);
+                l.node = l.node.next;
+            }
+            //Console.WriteLine($"b : {b}");
+            //Console.WriteLine($"c : {c}");
+            //Console.WriteLine($"d : {d}");
+            //Console.WriteLine($"e : {e}");
+      
+        }
+
+
+
     }
 }
--- leetcodeExplore/lib/MyLinkedList.cs
+++ leetcodeExplore/lib/MyLinkedList.cs
@@ -3,8 +3,123 @@ namespace leetcodeExplore.lib
 {
     public class MyLinkedList
     {
+        public SinglyListNode node;
+        public int Count = 0;
+        /** Initialize your data structure here. */
         public MyLinkedList()
         {
+
+        }
+
+        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        public int Get(int index)
+        {
+            if (Count != 0 && index < Count)
+            {
+                SinglyListNode tempList = node;
+                for (int i = 0; i < index; i++)
+                {
+                    tempList = tempList.next;
+                }
+                return tempList.val;
+            }
+            else
+                return -1;
+
+        }
+
+        /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+        public void AddAtHead(int val)
+        {
+            if (node == null)
+            {
+                node = new SinglyListNode(val);
+            }
+            else
+            {
+                SinglyListNode newHeadNode = new SinglyListNode(val);
+                newHeadNode.next = node;
+                this.node = newHeadNode;
+            }
+            Count++;
+        }
+        /** Append a node of value val to the last element of the linked list. */
+        public void AddAtTail(int val)
+        {
+            var curr = this.node;
+            while (curr.next != null)
+            {
+                curr = curr.next;
+            }
+            curr.next = new SinglyListNode(val);
+            Count++;
+        }
+
+        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        public void AddAtIndex(int index, int val)
+        {
+            var tempList = this.node;
+            if (index < Count)
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    if (i == (index - 1))
+                    {
+                        var curr = new SinglyListNode(val);
+                        curr.next = tempList.next;
+                        tempList.next = curr;
+                    }
+                    else
+                    {
+                        tempList = tempList.next;
+                    }
+                }
+            }
+            else if(index == 0){
+                this.AddAtHead(val);
+                return;
+            }
+            else if (index == Count){
+                this.AddAtTail(val);
+                return;
+            }
+            else
+                return;
+            Count++;
+        }
+
+        /** Delete the index-th node in the linked list, if the index is valid. */
+        public void DeleteAtIndex(int index)
+        {
+            var tempList = this.node;
+            if (index == 0)
+                this.node = this.node.next;
+            else if (index < Count){
+                for (int i = 0; i < index; i++)
+                {
+                    if (i == (index - 1))
+                    {
+                        var prev = tempList;
+                        tempList = tempList.next;
+                        prev.next = tempList.next;
+                    }
+                    else{
+                        tempList = tempList.next;
+                    }
+                }
+            }
+            else{
+                return;
+            }
+            Count--;
+        }
+
+        public class SinglyListNode
+        {
+            public int val;
+            public SinglyListNode next;
+            public SinglyListNode(int x) { val = x; }
         }
     }
+
 }
