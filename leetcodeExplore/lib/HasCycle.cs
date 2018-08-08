8cfc3b9676d1e3725c109215db76c19cff669dcf
liaowenming <liaowenming23@gmail.com>
2018-08-09 00:06:45 +0800

    1

---
+++ leetcodeExplore/lib/HasCycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace leetcodeExplore.lib
+{
+    public class HasCycle
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        public HasCycle(ListNode head)
+        {
+            bool[] test = new bool[256];
+            var x = head;
+          
+        }
+    }
+}
