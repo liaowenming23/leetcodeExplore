44eca1e5cebef4d912409d322473ad8a6d66d6e5
liaowenming <liaowenming23@gmail.com>
2019-08-18 00:03:37 +0800

    miss

--- leetcodeExploreTest/ArrayAndString/RemoveElementTest.cs
+++ leetcodeExploreTest/ArrayAndString/RemoveElementTest.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using leetcodeExplore.ArrayAndString;
+using Xunit;
+
 namespace leetcodeExploreTest.ArrayAndString
 {
     public class RemoveElementTest
     {
         public RemoveElementTest()
         {
+        }
+
+        [Theory]
+        [InlineData(new int[] { 3,2,2,3}, 3, 2)]
+        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5)]
+        public void RemoveTest(int[] input, int val, int expected)
+        {
+            var target = new RemoveElement();
+            var actual = target.Remove(input, val);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
