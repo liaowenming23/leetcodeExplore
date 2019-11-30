872c7319cf1539ecd57215c7c39d177ff1091e1e
liaowenming <liaowenming23@gmail.com>
2019-11-30 23:31:03 +0800

    add detect cycle

--- leetcodeExplore/LinkedList.cs
+++ leetcodeExplore/LinkedList.cs
@@ -150,6 +150,32 @@ namespace leetcodeExplore
       }
       return result;
     }
+
+    public ListNode DetectCycle (ListNode head)
+    {
+      if (head == null)
+        return null;
+
+      var slow = head;
+      var fast = head;
+      while (fast != null && fast.next != null)
+      {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast)
+          break;
+      }
+
+      if (fast == null || fast.next == null)
+        return null;
+      slow = head;
+      while (slow != fast)
+      {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      return fast;
+    }
   }
 
   public class ListNode
--- leetcodeExploreTest/LinkedListTest.cs
+++ leetcodeExploreTest/LinkedListTest.cs
@@ -47,5 +47,51 @@ namespace leetcodeExploreTest
 
       Assert.False (actual);
     }
+
+    [Fact]
+    public void DetectCycle_Test1 ()
+    {
+      ListNode node = new ListNode (3);
+      node.next = new ListNode (2);
+      node.next.next = new ListNode (0);
+      node.next.next.next = new ListNode (-4);
+      node.next.next.next.next = node.next;
+
+      var expected = node.next;
+
+      var actual = _target.DetectCycle (node);
+      Assert.Equal (expected, actual);
+    }
+
+    [Fact]
+    public void DetectCycle_Test2 ()
+    {
+      ListNode node = new ListNode (1);
+      node.next = new ListNode (2);
+      node.next.next = node.next;
+
+      var expected = node;
+
+      var actual = _target.DetectCycle (node);
+      Assert.Equal (expected, actual);
+    }
+
+    [Fact]
+    public void DetectCycle_Test3 ()
+    {
+      ListNode node = new ListNode (1);
+      node.next = new ListNode (2);
+      node.next.next = new ListNode (3);
+      node.next.next.next = new ListNode (4);
+      node.next.next.next.next = new ListNode (5);
+      node.next.next.next.next.next = new ListNode (6);
+      node.next.next.next.next.next.next = new ListNode (7);
+      node.next.next.next.next.next.next.next = node.next
+
+      var expected = node.next;
+
+      var actual = _target.DetectCycle (node);
+      Assert.Equal (expected, actual);
+    }
   }
 }
