5d98b80a53e9da207586650d34129bff820f838e
Liao WenMing <liaowenming@Liaos-MacBook-Pro.local>
2021-04-23 23:52:12 +0800

    add search BST

--- leetcodeExplore/BST.cs
+++ leetcodeExplore/BST.cs
@@ -6,12 +6,12 @@ namespace leetcodeExplore
 {
     public class BST
     {
-        public bool IsValidBST (TreeNode root)
+        public bool IsValidBST(TreeNode root)
         {
-            return ValidBST (root, null, null);
+            return ValidBST(root, null, null);
         }
 
-        private bool ValidBST (TreeNode node, int? min_limit, int? max_limit)
+        private bool ValidBST(TreeNode node, int? min_limit, int? max_limit)
         {
             if (node == null) return true;
 
@@ -20,58 +20,68 @@ namespace leetcodeExplore
             if (min_limit != null && val <= min_limit) return false;
             if (max_limit != null && val >= max_limit) return false;
 
-            if (!ValidBST (node.left, min_limit, val)) return false;
-            if (!ValidBST (node.right, val, max_limit)) return false;
+            if (!ValidBST(node.left, min_limit, val)) return false;
+            if (!ValidBST(node.right, val, max_limit)) return false;
 
             return true;
         }
 
-        public BST ()
+        public BST()
         {
 
         }
 
-        public BST (TreeNode root)
+        public BST(TreeNode root)
         {
-            var list = new List<int> ();
-            var stack = new Stack ();
+            var list = new List<int>();
+            var stack = new Stack();
 
+        }
+
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root is null) return null;
+            if (root.val == val) return root;
+            if (root.val > val) return SearchBST(root.left, val);
+            if (root.val < val) return SearchBST(root.right, val);
+
+            return null;
         }
     }
 
     public class BSTIterator
     {
         private Stack<TreeNode> _stack;
-        public BSTIterator (TreeNode root)
+        public BSTIterator(TreeNode root)
         {
-            _stack = new Stack<TreeNode> ();
-            this.leftToStack (root);
+            _stack = new Stack<TreeNode>();
+            this.leftToStack(root);
         }
 
-        private void leftToStack (TreeNode root)
+        private void leftToStack(TreeNode root)
         {
             while (root != null)
             {
-                _stack.Push (root);
+                _stack.Push(root);
                 root = root.left;
             }
         }
 
         /** @return the next smallest number */
-        public int Next ()
+        public int Next()
         {
-            var next_node = _stack.Pop ();
+            var next_node = _stack.Pop();
 
             if (next_node.right != null)
             {
-                this.leftToStack (next_node.right);
+                this.leftToStack(next_node.right);
             }
 
             return next_node.val;
         }
 
         /** @return whether we have a next smallest number */
-        public bool HasNext ()
+        public bool HasNext()
         {
             return _stack.Count > 0;
         }
--- leetcodeExploreTest/BSTTest.cs
+++ leetcodeExploreTest/BSTTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using leetcodeExplore;
 using leetcodeExplore.model;
 using Xunit;
@@ -8,93 +10,125 @@ namespace leetcodeExploreTest
     {
         private readonly BST _target;
 
-        public BSTTest ()
+        public BSTTest()
         {
-            _target = new BST ();
+            _target = new BST();
         }
 
         [Fact]
-        public void IsValidBST_Test1 ()
+        public void IsValidBST_Test1()
         {
-            var input = new TreeNode (2);
-            input.left = new TreeNode (1);
-            input.right = new TreeNode (3);
+            var input = new TreeNode(2);
+            input.left = new TreeNode(1);
+            input.right = new TreeNode(3);
 
             var expected = true;
 
-            var actual = _target.IsValidBST (input);
+            var actual = _target.IsValidBST(input);
 
-            Assert.Equal (expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void IsValidBST_Test2 ()
+        public void IsValidBST_Test2()
         {
-            var input = new TreeNode (5);
-            input.left = new TreeNode (1);
-            input.right = new TreeNode (4);
-            input.right.left = new TreeNode (3);
-            input.right.right = new TreeNode (6);
+            var input = new TreeNode(5);
+            input.left = new TreeNode(1);
+            input.right = new TreeNode(4);
+            input.right.left = new TreeNode(3);
+            input.right.right = new TreeNode(6);
 
             var expected = false;
 
-            var actual = _target.IsValidBST (input);
+            var actual = _target.IsValidBST(input);
 
-            Assert.Equal (expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void IsValidBST_Test3 ()
+        public void IsValidBST_Test3()
         {
-            var input = new TreeNode (1);
-            input.left = new TreeNode (1);
+            var input = new TreeNode(1);
+            input.left = new TreeNode(1);
 
             var expected = false;
 
-            var actual = _target.IsValidBST (input);
+            var actual = _target.IsValidBST(input);
 
-            Assert.Equal (expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void IsValidBST_Test4 ()
+        public void IsValidBST_Test4()
         {
-            var input = new TreeNode (10);
-            input.left = new TreeNode (5);
-            input.right = new TreeNode (11);
-            input.right.left = new TreeNode (6);
-            input.right.right = new TreeNode (12);
+            var input = new TreeNode(10);
+            input.left = new TreeNode(5);
+            input.right = new TreeNode(11);
+            input.right.left = new TreeNode(6);
+            input.right.right = new TreeNode(12);
 
             var expected = false;
 
-            var actual = _target.IsValidBST (input);
+            var actual = _target.IsValidBST(input);
 
-            Assert.Equal (expected, actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void searchBST_Test1()
+        {
+            var input = new TreeNode(4);
+            input.left = new TreeNode(2);
+            input.right = new TreeNode(7);
+            input.left.left = new TreeNode(1);
+            input.left.right = new TreeNode(3);
+
+            var expected = new TreeNode(2);
+            expected.left = new TreeNode(1);
+            expected.right = new TreeNode(3);
+            var actual = _target.SearchBST(input, 2);
+            Assert.Equal(GetTreeNode(expected), GetTreeNode(actual));
+        }
+
+        private List<int> GetTreeNode(TreeNode node)
+        {
+            var result = new List<int>();
+            SetValue(node, result);
+            return result;
+        }
+
+        private void SetValue(TreeNode node, List<int> data)
+        {
+            if (node is null)
+                return;
+            data.Add(node.val);
+            SetValue(node.left, data);
+            SetValue(node.right, data);
         }
     }
 
     public class BSTIteratorTest
     {
         [Fact]
-        public void Case1 ()
+        public void Case1()
         {
-            var input = new TreeNode (7);
-            input.left = new TreeNode (3);
-            input.right = new TreeNode (15);
-            input.right.left = new TreeNode (9);
-            input.right.right = new TreeNode (20);
+            var input = new TreeNode(7);
+            input.left = new TreeNode(3);
+            input.right = new TreeNode(15);
+            input.right.left = new TreeNode(9);
+            input.right.right = new TreeNode(20);
 
-            var target = new BSTIterator (input);
+            var target = new BSTIterator(input);
 
-            Assert.Equal (3, target.Next ());
-            Assert.Equal (7, target.Next ());
-            Assert.True (target.HasNext ());
-            Assert.Equal (9, target.Next ());
-            Assert.True (target.HasNext ());
-            Assert.Equal (15, target.Next ());
-            Assert.True (target.HasNext ());
-            Assert.Equal (20, target.Next ());
-            Assert.False (target.HasNext ());
+            Assert.Equal(3, target.Next());
+            Assert.Equal(7, target.Next());
+            Assert.True(target.HasNext());
+            Assert.Equal(9, target.Next());
+            Assert.True(target.HasNext());
+            Assert.Equal(15, target.Next());
+            Assert.True(target.HasNext());
+            Assert.Equal(20, target.Next());
+            Assert.False(target.HasNext());
         }
     }
 }
